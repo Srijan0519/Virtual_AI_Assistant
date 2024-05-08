9e39b59d47504ec77c5d4207556bca8193dd6d9c
devesh <devesh2517@gmail.com>
2024-05-08 12:32:39 +0530

    improved AI component and simulation effect

--- AI Virtual Teaching Assistant/app/AIcomponent.py
+++ AI Virtual Teaching Assistant/app/AIcomponent.py
@@ -1,26 +1,25 @@
 # from ak import GROQ_API_KEY 
 from groq import Groq
-from .key import system_promt_Devesh_1, system_promt_Devesh_2,GROQ_API_KEY,system_promt_srijan
+from .key import system_promt_1,GROQ_API_KEY,system_promt_srijan,system_promt2
 import os
-#GROQ_API_KEY = "123"
+
 os.environ["GROQ_API_KEY"] = GROQ_API_KEY
 
 client = Groq(
     api_key=os.environ.get("GROQ_API_KEY"),
 )
-#client = Groq()
-#you are Aset, a virtual assistant for USDC, an edtech startup. You are named after the Egyptian goddess Isis, a goddes of magic. Your role is to provide help to the users and provide them the best possible answers to the questions they ask. You are conversational and very warm and welcoming and answer every question but if the questions are not very edtech specific, you do bring it up and generate a suitable answer.
+
 system_message = {
     "role": "system",
-    "content": system_promt_Devesh_1
+    "content": system_promt_1
 }
 
-messages = [system_message]
-
 def get_ai_response(conversation_history):
-    messages = [system_message]
-
-    for part in conversation_history:
+    messages = []
+    for i, part in enumerate(conversation_history):
+        if i % 4 == 0:
+            messages.append(system_message)
+        
         if part.startswith("Human:"):
             role = "user"
             content = part[7:].strip()
@@ -29,23 +28,13 @@ def get_ai_response(conversation_history):
             content = part[11:].strip()
         else:
             continue
-
+        
         messages.append({"role": role, "content": content})
-
+    
     chat_completion = client.chat.completions.create(
         messages=messages,
-        model="llama3-8b-8192",
+        model="llama3-70b-8192",
     )
-
     assistant_response = chat_completion.choices[0].message.content
     return assistant_response
 
-# def get_ai_response(user_message):
-#     messages.append({"role": "user", "content": user_message})
-#     chat_completion = client.chat.completions.create(
-#         messages=messages,
-#         model="llama3-70b-8192",
-#     )
-#     assistant_response = chat_completion.choices[0].message.content
-#     messages.append({"role": "assistant", "content": assistant_response})
-#     return assistant_response
--- AI Virtual Teaching Assistant/app/static/scripts.js
+++ AI Virtual Teaching Assistant/app/static/scripts.js
@@ -1,18 +1,20 @@
 $(document).ready(function() {
 
+    // Function to handle form submission
     $('#chat-form').submit(function(event) {
         event.preventDefault();
         var userMessage = $('#user-input').val();
-
+        
         displayMessage('user', userMessage);
+
         $('#user-input').val('');
 
+        // AJAX request to send message to server
         $.ajax({
             url: '/api/chat',
             type: 'POST',
             data: { user_message: userMessage },
             success: function(response) {
-
                 displayMessage('bot', response.bot_response);
             },
             error: function(error) {
@@ -23,13 +25,44 @@ $(document).ready(function() {
 
     // Function to display messages in the chat box
     function displayMessage(sender, message) {
-        var messageClass = (sender === 'user')? 'user-message' : 'bot-response';
-        var messageElement = $('<li>', { class: messageClass, html: marked.parse(message) });
+        var messageClass = (sender === 'user') ? 'user-message' : 'bot-response';
+        var messageElement = $('<li>', { class: messageClass });
         $('#chat-box').append(messageElement);
+
+        if (sender === 'bot') {
+            typeMessage(messageElement, message);
+        } else {
+            messageElement.html(marked.parse(message));
+        }
+
         $('#chat-box').scrollTop($('#chat-box')[0].scrollHeight);
     }
 
-    // Function to handle new session button click
+    // Function to simulate typing effect
+    function typeMessage(messageElement, message) {
+        var currentText = '';
+        var messageHTML = marked.parse(message);
+
+        var typeInterval = setInterval(function() {
+            if (currentText === messageHTML) {
+                clearInterval(typeInterval);
+                return;
+            }
+
+            currentText = messageHTML.substring(0, currentText.length + 1);
+            messageElement.html(currentText);
+        }, 5); 
+    }
+
+    // Function to display messages in the chat box
+
+    // function displayMessage(sender, message) {
+    //     var messageClass = (sender === 'user')? 'user-message' : 'bot-response';
+    //     var messageElement = $('<li>', { class: messageClass, html: marked.parse(message) });
+    //     $('#chat-box').append(messageElement);
+    //     $('#chat-box').scrollTop($('#chat-box')[0].scrollHeight);
+    // }
+
     $('#new-session-btn').click(function() {
         var confirmedNewSession = confirm('Are you sure you want to start a new session? ');
         if (confirmedNewSession) {
@@ -38,6 +71,7 @@ $(document).ready(function() {
                 type: 'POST',
                 data: { previous_session_id: '{{ session.get("session_id") }}' },
                 success: function(response) {
+                    
                     $('#chat-box').empty();
                 },
                 error: function(error) {
@@ -53,8 +87,9 @@ $(document).ready(function() {
             url: '/chat-history',
             type: 'GET',
             success: function(data) {
+                
                 $('#chat-sessions').empty();
-        
+            
                 const sessionGroups = {};
                 for (const entry of data.chat_history) {
                     const sessionId = entry.session_id;
@@ -64,12 +99,13 @@ $(document).ready(function() {
                     sessionGroups[sessionId].push(entry);
                 }
             
+                
                 for (const sessionId in sessionGroups) {
                     const sessionContainer = $('<div>', { class: 'chat-session' });
                     const sessionHeader = $('<h3>', { text: `Session ID: ${sessionId}` });
                     sessionContainer.append(sessionHeader);
             
-        
+                    
                     for (const entry of sessionGroups[sessionId]) {
                         const messageClass = entry.message.startsWith('Human:') ? 'user-message' : 'bot-response';
                         const messageText = entry.message.split(': ')[1];
@@ -80,6 +116,7 @@ $(document).ready(function() {
                     $('#chat-sessions').append(sessionContainer);
                 }
             
+                
                 $('.chat-session').click(function() {
                     const sessionId = $(this).find('h3').text().split(':')[1].trim();
                     resumeSession(sessionId);
@@ -97,12 +134,14 @@ $(document).ready(function() {
         $('#chat-history-modal').hide();
     });
 
+
     $(window).click(function(event) {
         if ($(event.target).parents('#chat-history-modal').length === 0) {
             $('#chat-history-modal').hide();
         }
     });
 
+    // Function to resume a session
     function resumeSession(sessionId) {
         $('#chat-box').empty();
     
@@ -111,15 +150,54 @@ $(document).ready(function() {
             type: 'POST',
             data: { session_id: sessionId },
             success: function(data) {
-                for (const message of data.conversation_history) {
+                const conversationHistory = data.conversation_history;
+    
+                for (const message of conversationHistory) {
                     const messageClass = message.startsWith('Human:') ? 'user-message' : 'bot-response';
                     const messageElement = $('<li>', { class: messageClass, html: marked.parse(message.substr(7)) });
                     $('#chat-box').append(messageElement);
                 }
     
                 $('#chat-box').scrollTop($('#chat-box')[0].scrollHeight);
-
+    
                 $('#chat-history-modal').hide();
+    
+                $.ajax({
+                    url: '/start-new-session',
+                    type: 'POST',
+                    data: { previous_session_id: sessionId },
+                    success: function(response) {
+                        
+                        $('#chat-form').off('submit').submit(function(event) {
+                            event.preventDefault();
+                            var userMessage = $('#user-input').val();
+    
+                            displayMessage('user', userMessage);
+    
+                            $('#user-input').val('');
+    
+                            conversationHistory.push(`Human: ${userMessage}`);
+    
+                            $.ajax({
+                                url: '/api/chat',
+                                type: 'POST',
+                                data: { user_message: userMessage, conversation_history: conversationHistory.join('\n') },
+                                success: function(response) {
+                                    
+                                    conversationHistory.push(`Assistant: ${response.bot_response}`);
+    
+                                    displayMessage('bot', response.bot_response);
+                                },
+                                error: function(error) {
+                                    console.log(error);
+                                }
+                            });
+                        });
+                    },
+                    error: function(error) {
+                        console.log(error);
+                    }
+                });
             },
             error: function(error) {
                 console.log(error);
